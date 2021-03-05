8eb866a31f1167050f7b66b3e8002122bc6b9933
MaxGraey <maxgraey@gmail.com>
2021-03-05 14:39:44 +0200

    fix daysInMonth

--- assembly/es.ts
+++ assembly/es.ts
@@ -77,9 +77,10 @@ export function dayOfYear(year: i32, month: i32, day: i32): i32 {
 // modified of
 // https://github.com/tc39/proposal-temporal/blob/49629f785eee61e9f6641452e01e995f846da3a1/polyfill/lib/ecmascript.mjs#L2164
 export function daysInMonth(year: i32, month: i32): i32 {
-  return month == 2
+  month -= 1;
+  return month == 1
     ? 28 + i32(leapYear(year))
-    : 30 + ((month + i32(month >= 6)) & 1);
+    : 31 - ((month + i32(month >= 7)) & 1);
 }
 
 // https://github.com/tc39/proposal-temporal/blob/49629f785eee61e9f6641452e01e995f846da3a1/polyfill/lib/ecmascript.mjs#L2171
