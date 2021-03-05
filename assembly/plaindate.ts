b1f547a114dbe1faf9670111815a12f0d5d1a2d0
Max Graey <maxgraey@gmail.com>
2021-03-05 18:18:09 +0200

    Rename "es" to "utils". Add generic PlainDate.from. Add inlines and other minor refactorings (#7)
    * refactorings
    
    * more
    
    * inline fromPlainDate

--- assembly/plaindate.ts
+++ assembly/plaindate.ts
@@ -5,46 +5,85 @@ import {
   TimeComponent,
   addDate,
   dayOfWeek,
-  daysInMonth,
   Overflow,
+  dayOfYear,
   weekOfYear,
   balanceDuration,
-} from "./es";
-
-function toPaddedString(number: i32, length: i32 = 2): string {
-  return number.toString().padStart(length, "0");
-}
+  toPaddedString
+} from "./utils";
 
 export class PlainDate {
   readonly daysInYear: i32;
   readonly inLeapYear: bool;
 
+  @inline
+  static fromPlainDate(date: PlainDate): PlainDate {
+    return new PlainDate(date.year, date.month, date.day);
+  }
+
+  static fromString(date: string): PlainDate {
+    const dateRegex = new RegExp(
+      "^((?:[+-]\\d{6}|\\d{4}))(?:-(\\d{2})-(\\d{2})|(\\d{2})(\\d{2}))(?:(?:T|\\s+)(\\d{2})(?::(\\d{2})(?::(\\d{2})(?:[.,](\\d{1,9}))?)?|(\\d{2})(?:(\\d{2})(?:[.,](\\d{1,9}))?)?)?)?(?:([zZ])|(?:([+-])([01][0-9]|2[0-3])(?::?([0-5][0-9])(?::?([0-5][0-9])(?:[.,](\\d{1,9}))?)?)?)?)(?:\\[((?:(?:\\.\\.[-A-Za-z._]{1,12}|\\.[-A-Za-z_][-A-Za-z._]{0,12}|_[-A-Za-z._]{0,13}|[a-zA-Z](?:[A-Za-z._][-A-Za-z._]{0,12})?|[a-zA-Z]-(?:[-._][-A-Za-z._]{0,11})?|[a-zA-Z]-[a-zA-Z](?:[-._][-A-Za-z._]{0,10})?|[a-zA-Z]-[a-zA-Z][a-zA-Z](?:[A-Za-z._][-A-Za-z._]{0,9})?|[a-zA-Z]-[a-zA-Z][a-zA-Z]-(?:[-._][-A-Za-z._]{0,8})?|[a-zA-Z]-[a-zA-Z][a-zA-Z]-[a-zA-Z](?:[-._][-A-Za-z._]{0,7})?|[a-zA-Z]-[a-zA-Z][a-zA-Z]-[a-zA-Z][a-zA-Z](?:[-._][-A-Za-z._]{0,6})?)(?:\\/(?:\\.[-A-Za-z_]|\\.\\.[-A-Za-z._]{1,12}|\\.[-A-Za-z_][-A-Za-z._]{0,12}|[A-Za-z_][-A-Za-z._]{0,13}))*|Etc\\/GMT[-+]\\d{1,2}|(?:[+\\u2212-][0-2][0-9](?::?[0-5][0-9](?::?[0-5][0-9](?:[.,]\\d{1,9})?)?)?)))\\])?(?:\\[u-ca-((?:[A-Za-z0-9]{3,8}(?:-[A-Za-z0-9]{3,8})*))\\])?$",
+      "i"
+    );
+    const match = dateRegex.exec(date);
+    if (match != null) {
+      return new PlainDate(
+        I32.parseInt(match.matches[1]),
+        // see https://github.com/ColinEberhardt/assemblyscript-regex/issues/38
+        I32.parseInt(
+          match.matches[2] != "" ? match.matches[2] : match.matches[19]
+        ),
+        I32.parseInt(
+          match.matches[3] != "" ? match.matches[3] : match.matches[20]
+        )
+      );
+    }
+    throw new RangeError("invalid ISO 8601 string: " + date);
+  }
+
+  @inline
+  static from<T>(date: T): PlainDate {
+    if (isString<T>()) {
+      // @ts-ignore: cast
+      return this.fromString(<string>date);
+    }
+    if (isReference<T>()) {
+      if (date instanceof PlainDate) {
+        return new PlainDate(date.year, date.month, date.day);
+      }
+    }
+    throw new TypeError("invalid date type");
+  }
+
   constructor(readonly year: i32, readonly month: i32, readonly day: i32) {}
 
+  @inline
   get dayOfWeek(): i32 {
     return dayOfWeek(this.year, this.month, this.day);
   }
 
+  @inline
   get dayOfYear(): i32 {
-    let days = this.day;
-    for (let m = this.month - 1; m > 0; m--) {
-      days += daysInMonth(this.year, m);
-    }
-    return days;
+    return dayOfYear(this.year, this.month, this.day);
   }
 
+  @inline
   get weekOfYear(): i32 {
     return weekOfYear(this.year, this.month, this.day);
   }
 
+  @inline
   get daysInWeek(): i32 {
     return 7;
   }
 
+  @inline
   get monthsInYear(): i32 {
     return 12;
   }
 
+  @inline
   get monthCode(): string {
     return (this.month >= 10 ? "M" : "M0") + this.month.toString();
   }
@@ -59,6 +98,7 @@ export class PlainDate {
     );
   }
 
+  @inline
   equals(date: PlainDate): bool {
     return (
       this.day   == date.day   &&
@@ -137,30 +177,5 @@ export class PlainDate {
       return 1;
     }
     return 0;
-  }
-
-  static fromPlainDate(date: PlainDate): PlainDate {
-    return new PlainDate(date.year, date.month, date.day);
-  }
-
-  static fromString(date: string): PlainDate {
-    const dateRegex = new RegExp(
-      "^((?:[+-]\\d{6}|\\d{4}))(?:-(\\d{2})-(\\d{2})|(\\d{2})(\\d{2}))(?:(?:T|\\s+)(\\d{2})(?::(\\d{2})(?::(\\d{2})(?:[.,](\\d{1,9}))?)?|(\\d{2})(?:(\\d{2})(?:[.,](\\d{1,9}))?)?)?)?(?:([zZ])|(?:([+-])([01][0-9]|2[0-3])(?::?([0-5][0-9])(?::?([0-5][0-9])(?:[.,](\\d{1,9}))?)?)?)?)(?:\\[((?:(?:\\.\\.[-A-Za-z._]{1,12}|\\.[-A-Za-z_][-A-Za-z._]{0,12}|_[-A-Za-z._]{0,13}|[a-zA-Z](?:[A-Za-z._][-A-Za-z._]{0,12})?|[a-zA-Z]-(?:[-._][-A-Za-z._]{0,11})?|[a-zA-Z]-[a-zA-Z](?:[-._][-A-Za-z._]{0,10})?|[a-zA-Z]-[a-zA-Z][a-zA-Z](?:[A-Za-z._][-A-Za-z._]{0,9})?|[a-zA-Z]-[a-zA-Z][a-zA-Z]-(?:[-._][-A-Za-z._]{0,8})?|[a-zA-Z]-[a-zA-Z][a-zA-Z]-[a-zA-Z](?:[-._][-A-Za-z._]{0,7})?|[a-zA-Z]-[a-zA-Z][a-zA-Z]-[a-zA-Z][a-zA-Z](?:[-._][-A-Za-z._]{0,6})?)(?:\\/(?:\\.[-A-Za-z_]|\\.\\.[-A-Za-z._]{1,12}|\\.[-A-Za-z_][-A-Za-z._]{0,12}|[A-Za-z_][-A-Za-z._]{0,13}))*|Etc\\/GMT[-+]\\d{1,2}|(?:[+\\u2212-][0-2][0-9](?::?[0-5][0-9](?::?[0-5][0-9](?:[.,]\\d{1,9})?)?)?)))\\])?(?:\\[u-ca-((?:[A-Za-z0-9]{3,8}(?:-[A-Za-z0-9]{3,8})*))\\])?$",
-      "i"
-    );
-    const match = dateRegex.exec(date);
-    if (match != null) {
-      return new PlainDate(
-        I32.parseInt(match.matches[1]),
-        // see https://github.com/ColinEberhardt/assemblyscript-regex/issues/38
-        I32.parseInt(
-          match.matches[2] != "" ? match.matches[2] : match.matches[19]
-        ),
-        I32.parseInt(
-          match.matches[3] != "" ? match.matches[3] : match.matches[20]
-        )
-      );
-    }
-    throw new RangeError("invalid ISO 8601 string: " + date);
   }
 }
