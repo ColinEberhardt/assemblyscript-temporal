8fbedbd0ae87091134c73cf486cee0493b2114ba
MaxGraey <maxgraey@gmail.com>
2021-03-05 17:23:51 +0200

    refactor

--- assembly/es.ts
+++ assembly/es.ts
@@ -167,10 +167,8 @@ export function clamp(value: i32, lo: i32, hi: i32): i32 {
 }
 
 // https://github.com/tc39/proposal-temporal/blob/51c6c5138b5b73817f5e0ff2694fe0134f09b0a7/polyfill/lib/ecmascript.mjs#L2704
-export function checkRange(value: i32, lo: i32, hi: i32): void {
-  if (u32(value - lo) > u32(hi - lo)) {
-    throw new RangeError("value out of range");
-  }
+export function checkRange(value: i32, lo: i32, hi: i32): bool {
+  return u32(value - lo) <= u32(hi - lo);
 }
 
 // https://github.com/tc39/proposal-temporal/blob/49629f785eee61e9f6641452e01e995f846da3a1/polyfill/lib/ecmascript.mjs#L2617
--- assembly/plaintime.ts
+++ assembly/plaintime.ts
@@ -2,21 +2,21 @@ import { checkRange } from "./es";
 
 export class PlainTime {
   constructor(
-    readonly hour: i32   = 0,
-    readonly minute: i32 = 0,
-    readonly second: i32 = 0,
+    readonly hour: i32        = 0,
+    readonly minute: i32      = 0,
+    readonly second: i32      = 0,
     readonly millisecond: i32 = 0,
     readonly microsecond: i32 = 0,
     readonly nanosecond: i32  = 0
   ) {
-    this.validate(
-      hour,
-      minute,
-      second,
-      millisecond,
-      microsecond,
-      nanosecond
-    );
+    if (!(
+      checkRange(hour,        0,  23) &&
+      checkRange(minute,      0,  59) &&
+      checkRange(second,      0,  59) &&
+      checkRange(millisecond, 0, 999) &&
+      checkRange(microsecond, 0, 999) &&
+      checkRange(nanosecond,  0, 999)
+    )) throw new RangeError("invalid plain time");
   }
 
   equals(other: PlainTime): bool {
@@ -28,23 +28,5 @@ export class PlainTime {
       this.minute == other.minute &&
       this.hour   == other.hour
     );
-  }
-
-  @inline
-  private validate(
-    hour: i32,
-    minute: i32,
-    second: i32,
-    millisecond: i32,
-    microsecond: i32,
-    nanosecond: i32
-  ): void {
-    checkRange(hour,   0, 23);
-    checkRange(minute, 0, 59);
-    checkRange(second, 0, 59);
-
-    checkRange(millisecond, 0, 999);
-    checkRange(microsecond, 0, 999);
-    checkRange(nanosecond,  0, 999);
   }
 }
