e39923e185925f90d1c54e3fc7bb3144687d7564
MaxGraey <maxgraey@gmail.com>
2021-03-12 20:28:31 +0200

    refactor Duration.toString

--- assembly/duration.ts
+++ assembly/duration.ts
@@ -1,11 +1,3 @@
-function componentString(value: i32, suffix: string): string {
-  return value != 0 ? value.toString() + suffix : "";
-}
-
-function fcomponentString(value: f32, suffix: string): string {
-  return value != 0 ? value.toString() + suffix : "";
-}
-
 export class Duration {
   constructor(
     public years: i32 = 0,
@@ -22,24 +14,30 @@ export class Duration {
 
   // P1Y1M1DT1H1M1.1S
   toString(): string {
-    const dayComponent =
-      componentString(this.years, "Y") +
-      componentString(this.months, "M") +
-      componentString(this.days, "D") +
-      componentString(this.weeks, "W");
-    const timeComponent =
-      componentString(this.hours, "H") +
-      componentString(this.minutes, "M") +
-      fcomponentString(
-        f32(this.seconds) +
-          f32(this.milliseconds) / 1000.0 +
-          f32(this.microseconds) / 1000000.0 +
-          f32(this.nanoseconds) / 1000000000.0,
-        "S"
-      );
-
-    return (
-      "P" + dayComponent + (timeComponent != "" ? "T" + timeComponent : "")
+    const data = (
+      toString(this.years,  "Y") +
+      toString(this.months, "M") +
+      toString(this.days,   "D") +
+      toString(this.weeks,  "W")
     );
+
+    const time = (
+      toString(this.hours,   "H") +
+      toString(this.minutes, "M") +
+      toString(
+        // sort in ascending order for better sum precision
+        f64(this.nanoseconds)  / 1000000000.0 +
+        f64(this.microseconds) / 1000000.0 +
+        f64(this.milliseconds) / 1000.0 +
+        f64(this.seconds),
+        "S"
+      )
+    );
+
+    return "P" + data + (time.length ? "T" + time : "");
   }
+}
+
+function toString<T extends number>(value: T, suffix: string): string {
+  return value ? value.toString() + suffix : "";
 }
